971f4f8c8db87531f4f78092cce3173d4c89c43d
seungho Jeon <urijsh612@gmail.com>
2020-03-27 23:18:41 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  switch (action.type) {
+    case "ADD":
+      return count + 1;
+    case "MINUS":
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" }); // 1
+countStore.dispatch({ type: "ADD" }); // 2
+countStore.dispatch({ type: "ADD" }); // 3
+countStore.dispatch({ type: "ADD" }); // 4
+countStore.dispatch({ type: "ADD" }); // 5
+countStore.dispatch({ type: "MINUS" }); // 4
 
 console.log(countStore.getState());
