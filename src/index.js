70a5992aa1c2cdcd2aaca73cee3802e66a1e932c
seungho Jeon <urijsh612@gmail.com>
2020-03-28 16:51:33 +0900

    #3.X react redux (hooks 로 접근하나 여기선 connect 사용)

--- src/index.js
+++ src/index.js
@@ -44,76 +44,90 @@
 // minus.addEventListener("click", handleMinus);
 
 // #2. 리덕스 todo List
-import { createStore } from "redux";
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+// import { createStore } from "redux";
+// const form = document.querySelector("form");
+// const input = document.querySelector("input");
+// const ul = document.querySelector("ul");
 
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
+// const ADD_TODO = "ADD_TODO";
+// const DELETE_TODO = "DELETE_TODO";
 
-const addToDo = text => {
-  return {
-    type: ADD_TODO,
-    text
-  };
-};
+// const addToDo = text => {
+//   return {
+//     type: ADD_TODO,
+//     text
+//   };
+// };
 
-const deleteToDo = id => {
-  return {
-    type: DELETE_TODO,
-    id
-  };
-};
+// const deleteToDo = id => {
+//   return {
+//     type: DELETE_TODO,
+//     id
+//   };
+// };
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      const newDoToObj = { text: action.text, id: Date.now() };
-      return [newDoToObj, ...state];
-    case DELETE_TODO:
-      const cleaned = state.filter(toDo => toDo.id !== action.id);
-      return cleaned;
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case ADD_TODO:
+//       const newDoToObj = { text: action.text, id: Date.now() };
+//       return [newDoToObj, ...state];
+//     case DELETE_TODO:
+//       const cleaned = state.filter(toDo => toDo.id !== action.id);
+//       return cleaned;
+//     default:
+//       return state;
+//   }
+// };
 
-const store = createStore(reducer);
+// const store = createStore(reducer);
 
-store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => console.log(store.getState()));
 
-const dispatchAddToDo = text => {
-  store.dispatch(addToDo(text));
-};
+// const dispatchAddToDo = text => {
+//   store.dispatch(addToDo(text));
+// };
 
-const dispatchDeleteToDo = e => {
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch(deleteToDo(id));
-};
+// const dispatchDeleteToDo = e => {
+//   const id = parseInt(e.target.parentNode.id);
+//   store.dispatch(deleteToDo(id));
+// };
 
-const paintToDos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = "";
-  toDos.forEach(toDo => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", dispatchDeleteToDo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
+// const paintToDos = () => {
+//   const toDos = store.getState();
+//   ul.innerHTML = "";
+//   toDos.forEach(toDo => {
+//     const li = document.createElement("li");
+//     const btn = document.createElement("button");
+//     btn.innerText = "DEL";
+//     btn.addEventListener("click", dispatchDeleteToDo);
+//     li.id = toDo.id;
+//     li.innerText = toDo.text;
+//     li.appendChild(btn);
+//     ul.appendChild(li);
+//   });
+// };
 
-store.subscribe(paintToDos);
+// store.subscribe(paintToDos);
 
-const onSubmit = e => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  dispatchAddToDo(toDo);
-};
+// const onSubmit = e => {
+//   e.preventDefault();
+//   const toDo = input.value;
+//   input.value = "";
+//   dispatchAddToDo(toDo);
+// };
 
-form.addEventListener("submit", onSubmit);
+// form.addEventListener("submit", onSubmit);
+
+// 리액트와 redux
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App></App>
+  </Provider>,
+  document.getElementById("root")
+);
